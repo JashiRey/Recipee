13a018eb92478c013b4ab09c3ed9f4d12b2f0894
Syphi-x <erick.327.ru@gmail.com>
2024-11-28 11:07:54 -0600

    Update start.php

--- api/start.php
+++ api/start.php
@@ -5,7 +5,16 @@
     
   header("Content-Type: application/json"); // Asegura que la respuesta sea JSON
 
+<<<<<<< Updated upstream
   $db = new mysqli('localhost', 'root', '', 'recipee');
+=======
+  try {
+    $db = new mysqli('localhost', 'root', '', 'recipee');
+    echo "Conexión exitosa";
+  } catch (Exception $e) {
+      echo "Error: " . $e->getMessage();
+  }
+>>>>>>> Stashed changes
 
   if(!$db) {
     die('Connection failed: ' . mysqli_connect_error());
